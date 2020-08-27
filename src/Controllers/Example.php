bfc39bcb00ba063cb96e489ea57c630c7cd49c52
Oliver Nossol <o.nossol@nexus-united.com>
2020-08-27 15:14:01 +0200

    adapted composer.json file

---
+++ src/Controllers/Example.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controllers;
+
+class Example
+{
+    public function doSomethingElse()
+    {
+        echo nl2br('Ich bin die Example Klasse');
+    }
+}
+
