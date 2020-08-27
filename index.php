a541e1f5ac8765aa4670b6c19531e03da185d4af
Oliver Nossol <o.nossol@nexus-united.com>
2020-08-27 15:14:25 +0200

    adapted composer.json file

--- index.php
+++ index.php
@@ -2,11 +2,18 @@
 
 declare(strict_types=1);
 
-require 'Controllers/Foo.php';
+ini_set('display_errors', '1');
+ini_set('display_startup_errors', '1');
+error_reporting(E_ALL);
 
-use mvc\Controllers\Foo;
+require 'vendor/autoload.php';
+
+use App\Controllers\Foo;
+use App\Controllers\Example;
 
 $foo = new Foo();
-
 $foo->doSomething();
+
+$example = new Example();
+$example->doSomethingElse();
 
--- src/Controllers/Foo.php
+++ src/Controllers/Foo.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace mvc\Controllers;
+namespace App\Controllers;
 
 use DateTime;
 
