dac39d6a7e2f00ce00c93434587a617883124ff6
Oliver Nossol <o.nossol@nexus-united.com>
2020-09-10 11:07:40 +0200

    start with namespaces...again

--- src/Controllers/fruits.php
+++ src/Controllers/fruits.php
@@ -1,3 +1,4 @@
 <?php
 
+
 require 'src/Views/fruits.view.php';
--- src/Models/Bootstrap.php
+++ src/Models/Bootstrap.php
@@ -1,13 +1,6 @@
 <?php
 
 $app = [];
-//$app['config'] = require 'src/config.php';
 
 require 'src/Models/Router.php';
 require 'src/Models/Request.php';
-//require 'src/Models/Connection.php';
-//require 'src/Models/QueryBuilder.php';
-
-//$app['database'] = new QueryBuilder(
-//    Connection::make($app['config']['database'])
-//);
