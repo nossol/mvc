8f02b55b16f2aca13d523d3c50e971d8c56a23de
Oliver Nossol <o.nossol@nexus-united.com>
2020-09-10 15:44:41 +0200

    added namespaces

--- index.php
+++ index.php
@@ -6,12 +6,11 @@ ini_set('display_errors', '1');
 ini_set('display_startup_errors', '1');
 error_reporting(E_ALL);
 
+use App\Core\{Router, Request};
+
 require 'vendor/autoload.php';
 require 'src/core/Bootstrap.php';
 
-require Router::load('Routes.php')
+require Router::load('src/core/Routes.php')
     ->direct(Request::uri());
-
-
-
 
--- src/core/Router.php
+++ src/core/Router.php
@@ -1,5 +1,7 @@
 <?php
 
+namespace App\core;
+
 class Router
 {
     protected $routes = [];
