9341bf4da07d87cb08d758ca174804f5bb78d349
Oliver Nossol <o.nossol@nexus-united.com>
2021-02-05 18:13:01 +0100

    - added fallback

--- index.php
+++ index.php
@@ -7,11 +7,11 @@ ini_set('display_errors', '1');
 ini_set('display_startup_errors', '1');
 error_reporting(E_ALL);
 
+use App\Controller\Error;
 use App\Service\View;
 use App\Model\ProductRepository;
 use App\Service\Container;
 use App\Service\ControllerProvider;
-
 
 require_once __DIR__ . '/vendor/autoload.php';
 
@@ -20,19 +20,24 @@ $productRepository = new ProductRepository();
 $container = new Container();
 $container->set(View::class, new View());
 
-$controller = new ControllerProvider();
+$controllerProvider = new ControllerProvider();
 $page = 'home';
 if (!empty($_GET['page'])) {
     $page = $_GET['page'];
 }
 
-$controllerList = $controller->getList();
+$controllerList = $controllerProvider->getList();
 
-foreach ($controllerList as $controller) {
-    if (strtolower($controller::ROUTE) === $page) {
-        $controller = new $controller($container, $productRepository);
+foreach ($controllerList as $viewController) {
+    if (strtolower($viewController::ROUTE) === $page) {
+        $controller = new $viewController($container, $productRepository);
         $controller->action();
     }
+}
+
+if (!isset($controller)) {
+    $controller = new Error($container);
+    $controller->action();
 }
 
 $view = $container->get(View::class);
