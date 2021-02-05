654c2dbc70d7d4f636e9d5823b11d3806ef95f2b
Oliver Nossol <o.nossol@nexus-united.com>
2021-02-05 10:34:25 +0100

    - added DTO

---
+++ src/Model/Dto/ProductDataTransferObject.php
@@ -0,0 +1,69 @@
+<?php
+
+
+namespace App\Model\Dto;
+
+
+class ProductDataTransferObject
+{
+    private int $id;
+    private string $name;
+    private string $description;
+
+    public function __construct()
+    {
+        $this->id = 0;
+        $this->name = '';
+        $this->description = '';
+    }
+
+    /**
+     * @return int
+     */
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param int $id
+     */
+    public function setId(int $id): void
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string $name
+     */
+    public function setName(string $name): void
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @return string
+     */
+    public function getDescription(): string
+    {
+        return $this->description;
+    }
+
+    /**
+     * @param string $description
+     */
+    public function setDescription(string $description): void
+    {
+        $this->description = $description;
+    }
+
+
+}
